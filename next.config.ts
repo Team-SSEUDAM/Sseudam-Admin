412bc9253c98c52825266e08807d8b0b014ac0d7
hjy0951 <hjy0951@gmail.com>
2025-06-20 22:26:52 +0900

    chore: localhost https 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import { readFileSync } from "fs";
+import { join } from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  server: {
+    https: {
+      key: readFileSync(
+        join(process.cwd(), process.env.NEXT_PUBLIC_HTTPS_PEM_KEY || "")
+      ),
+      cert: readFileSync(
+        join(process.cwd(), process.env.NEXT_PUBLIC_HTTPS_PEM || "")
+      ),
+    },
+  },
 };
 
 export default nextConfig;
